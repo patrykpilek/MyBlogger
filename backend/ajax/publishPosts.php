e0a26b84113b4741c44405cb57aa84ce528e87e7
Patryk Pilek <patryk.pilek@gmail.com>
2023-01-22 22:56:37 +0000

    creating condition to publish posts at once

--- backend/ajax/publishPosts.php
+++ backend/ajax/publishPosts.php
@@ -1,3 +1,30 @@
 <?php
 
-//publish posts
+include '../init.php';
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    if(isset($_POST['postIDs'])) {
+        $postIDs = json_decode($_POST['postIDs']);
+        $blogIDs = json_decode($_POST['blogIDs']);
+
+        if(!empty($postIDs) && !empty($blogIDs)) {
+            foreach ($postIDs as $postID) {
+                foreach ($blogIDs as $blogID) {
+                    $post = $userObj->get("posts", ['postID' => $postID, 'blogID' => $blogID]);
+                    $blog = $dashObj->blogAuth($post->blogID);
+
+                    if($blog) {
+                        if($blog->role === "Admin" OR $blog->userID === $post->authorID) {
+                            //update post
+                        } else {
+                            echo "Your don'\t have rights to preform this action!";
+                        }
+                    } else {
+                        echo 'Something went wrong!';
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
